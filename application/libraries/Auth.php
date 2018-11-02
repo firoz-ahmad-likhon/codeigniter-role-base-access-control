a02d55c56ba2e8cbdfeba9d4c9db329af2360985
Likhon <likh.deshi@gmail.com>
2018-11-03 04:24:42 +0600

    Url correction

--- application/libraries/Auth.php
+++ application/libraries/Auth.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Author: Firoz Ahmad Likhon <likh.deshi@gmail.com>
- * Website: https://github.com/likhonlikh
+ * Website: https://github.com/firoz-ahmad-likhon
  *
  * Copyright (c) 2018 Firoz Ahmad Likhon
  * Released under the MIT license
